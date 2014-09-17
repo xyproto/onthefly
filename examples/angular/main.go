70d6465b7021dccf32cddd97b8795c0771336cd4
Alexander F Rødseth <rodseth@gmail.com>
2014-09-17 21:01:17 +0200

    Added an example for AngularJS and Negroni

---
+++ examples/angular/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/xyproto/onthefly"
+)
+
+const (
+	angular_version = "1.3.0-rc.2"
+)
+
+// Generate a new onthefly Page (HTML5, Angular and CSS combined)
+func indexPage() *onthefly.Page {
+
+	// Create a new HTML5 page, with CSS included
+	page := onthefly.NewAngularPage("Demonstration", angular_version)
+
+	// Rely on the body tag being present
+	body, _ := page.GetTag("body")
+
+	// Add a title paragraph
+	title := body.AddNewTag("p")
+	// Use id attributes to style similar elements separately
+	title.AddAttrib("id", "title")
+	title.AddContent(fmt.Sprintf("onthefly %.1f & angular %s", onthefly.Version, angular_version))
+	title.AddStyle("font-size", "2em")
+	title.AddStyle("font-family", "sans-serif")
+	title.AddStyle("font-style", "italic")
+
+	// Add a paragraph for the angular demonstration code
+	angularp := body.AddNewTag("p")
+	angularp.AddAttrib("id", "angular")
+	angularp.AddStyle("margin-top", "2em")
+
+	// Label for the input box
+	label := angularp.AddNewTag("label")
+	inputID := "input1"
+	label.AddAttrib("for", inputID)
+	label.AddContent("Input text:")
+	label.AddStyle("margin-right", "3em")
+
+	// Angular input
+	input := angularp.AddNewTag("input")
+	input.AddAttrib("id", inputID)
+	input.AddAttrib("type", "text")
+	input.AddAttrib("ng-model", "sometext")
+
+	// Angular output
+	h1 := angularp.AddNewTag("h1")
+	h1.AddAttrib("ng-show", "sometext")
+	h1.AddContent("HELLO {{ sometext | uppercase }}")
+	h1.AddStyle("color", "red")
+	h1.AddStyle("margin", "2em")
+	h1.AddStyle("font-size", "4em")
+
+	// Set the margin (em is default)
+	page.SetMargin(5)
+
+	// Set the font family
+	page.SetFontFamily("serif")
+
+	// Set the color scheme (fg, bg)
+	page.SetColor("black", "#e0e0e0")
+
+	return page
+}
+
+// Set up the paths and handlers then start serving.
+func main() {
+	fmt.Println("onthefly ", onthefly.Version)
+
+	// Create a Negroni instance and a ServeMux instance
+	n := negroni.Classic()
+	mux := http.NewServeMux()
+
+	// Publish the generated Page in a way that connects the HTML and CSS. Cached.
+	indexPage().Publish(mux, "/", "/style.css", false)
+
+	// Handler goes last
+	n.UseHandler(mux)
+
+	// Listen for requests at port 8080
+	n.Run(":8080")
+}
